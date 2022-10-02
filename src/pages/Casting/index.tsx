af55d5364a746f25f686c6618f5b48f7c16a8960
gengyi <609924321@qq.com>
2022-10-02 14:52:52 +0800

    feat:

--- src/pages/Casting/index.tsx
+++ src/pages/Casting/index.tsx
@@ -5,13 +5,34 @@ import {
   Input
 } from 'antd'
 import styles from './index.module.less';
+import uploadIcon from '../../assets/images/upload.png';
 
+const { TextArea } = Input;
 const Casting = () => {
-  const [imageUrl, setImageUrl] = useState<string>('');
-  const [loading, setLoading] = useState<boolean>(false);
+  const [loading, setLoading] = useState(false);
+  const [imageUrl, setImageUrl] = useState<string>();
+
   const uploadButton = (
     <div>
-      {loading ? <LoadingOutlined /> : <PlusOutlined />}
+      <img
+        src={uploadIcon}
+        style={{
+          width: '63px',
+          height: '63px'
+        }}
+      />
+    </div>
+  );
+
+  const uploadAction = (
+    <div>
+      <img
+        src={uploadIcon}
+        style={{
+          width: '36px',
+          height: '36px'
+        }}
+      />
     </div>
   );
   return (
@@ -26,7 +47,7 @@ const Casting = () => {
               <Upload
                 name="avatar"
                 listType="picture-card"
-                className={styles['avatar-uploader']}
+                className={styles['avatar-uploade']}
                 showUploadList={false}
                 action=""
               >
@@ -43,6 +64,48 @@ const Casting = () => {
             </div>
           )
         }
+      </div>
+      <div className={styles.casting_view_main}>
+        <div className={styles.casting_view_main_control}>
+          <div className={styles.casting_view_main_control_left}>
+            <div className={styles.casting_view_main_control_item}>
+              <h4 className={styles.casting_view_main_control_item_title}>出售价格</h4>
+              <Input
+                placeholder='请输入该商品的出售价格'
+                className={styles.casting_view_main_control_item_value}
+              />
+            </div>
+            <div className={styles.casting_view_main_control_item}>
+              <h4 className={styles.casting_view_main_control_item_title}>NFT 名称</h4>
+              <Input
+                placeholder='请输入NFT的名称'
+                className={styles.casting_view_main_control_item_value}
+              />
+            </div>
+            <div className={styles.casting_view_main_control_item}>
+              <h4 className={styles.casting_view_main_control_item_title}>NFT 介绍</h4>
+              <TextArea
+                placeholder='请输入NFT的介绍'
+                className={styles.casting_view_main_control_item_textarea}
+              />
+            </div>
+          </div>
+          <div className={styles.casting_view_main_control_right}>
+            <div className={styles.casting_view_main_control_item}>
+              <h4 className={styles.casting_view_main_control_item_title}>上传封面</h4>
+              <Upload
+                name="avatar"
+                listType="picture-card"
+                className={styles['avatar-uploader']}
+                showUploadList={false}
+                action=""
+              >
+                {imageUrl ? <img src={imageUrl} alt="avatar" style={{ width: '100%' }} /> : uploadAction}
+              </Upload>
+              <p className={styles.casting_view_main_control_item_tip}>( 支持JPEG、PNG图片文件，文件大小 2MB以内 )</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
